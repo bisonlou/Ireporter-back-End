de0004257fa38d152ef74862312dd6c6d0bd349a
Innocent Lou <bisonlou@gmail.com>
2018-12-20 14:39:34 +0300

    added get all files to models

--- api/models.py
+++ api/models.py
@@ -7,6 +7,8 @@ def add_red_flag(red_flag):
 
 def get_current_id():
     if len(red_flags) == 0:
-        return 0
-        
+        return 0        
     return red_flags[-1]['id']
+
+def get_all_red_flags():
+    return red_flags
