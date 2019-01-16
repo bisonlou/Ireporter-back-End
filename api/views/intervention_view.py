6a8782732097fe65de19ef2362367f21c6a166ea
Innocent Lou <bisonlou@gmail.com>
2019-01-16 11:38:22 +0300

    removed duplication in views

--- api/views/intervention_view.py
+++ api/views/intervention_view.py
@@ -4,91 +4,67 @@ from flask import request, jsonify
 from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
 
-intervention_controller = IncidentController()
+incident_controller = IncidentController()
 incident_type = 'intervention'
 
 
 @app.route('/api/v1/interventions', methods=['POST'])
 @jwt_required
 def add_intervention():
-    data = request.get_json()
-    user_id = get_jwt_identity()
-    kwags = dict(data=data, user_id=user_id, incident_type=incident_type)
 
-    return intervention_controller.create_incident(**kwags)
+    return incident_controller.create_incident(incident_type)
 
 
 @app.route('/api/v1/interventions', methods=['GET'])
 @jwt_required
 def get_interventions():
-    user_id = get_jwt_identity()
-    kwags = dict(user_id=user_id, incident_type=incident_type)
 
-    return intervention_controller.get_all_incidents(**kwags)
+    return incident_controller.get_incidents(incident_type)
 
 
 @app.route('/api/v1/interventions/<int:incident_id>', methods=['GET'])
 @jwt_required
-def get_intervention(incident_id):
-    user_id = get_jwt_identity()
-    kwags = dict(incident_id=incident_id,
-                 user_id=user_id, incident_type=incident_type)
+def get_intervention(incident_id):    
 
-    return intervention_controller.get_incident(**kwags)
+    return incident_controller.get_incident(incident_type, incident_id)
 
 
 @app.route('/api/v1/interventions/<int:incident_id>', methods=['PUT'])
 @jwt_required
-def alter_intervention(incident_id):
-    data = request.get_json()
-    user_id = get_jwt_identity()
+def put_intervention(incident_id):
 
-    kwags = dict(data=data, user_id=user_id,
-                 incident_type=incident_type, incident_id=incident_id)
-
-    return intervention_controller.put_incident(**kwags)
+    return incident_controller.put_incident(incident_type, incident_id)
 
 
 @app.route('/api/v1/interventions/<int:incident_id>/location',
            methods=['PATCH'])
 @jwt_required
 def update_intervention_location(incident_id):
-    data = request.get_json()
-    user_id = get_jwt_identity()
-    kwags = dict(data=data, user_id=user_id,
-                 incident_type=incident_type, incident_id=incident_id,
-                 query='location')
 
-    return intervention_controller.patch_incident(**kwags)
+    return incident_controller.patch_incident(incident_type, incident_id,
+                                                  'location')
+
 
 @app.route('/api/v1/interventions/<int:incident_id>/comment',
            methods=['PATCH'])
 @jwt_required
 def update_intervention_comment(incident_id):
-    data = request.get_json()
-    user_id = get_jwt_identity()
-    kwags = dict(data=data, user_id=user_id,
-                 incident_type=incident_type, incident_id=incident_id,
-                 query='comment')
 
-    return intervention_controller.patch_incident(**kwags)
+    return incident_controller.patch_incident(incident_type, incident_id,
+                                                  'comment')
 
 
 @app.route('/api/v1/interventions/<int:incident_id>', methods=['DELETE'])
 @jwt_required
 def delete_intervention(incident_id):
-    user_id = get_jwt_identity()
-    kwags = dict(user_id=user_id, incident_type=incident_type,
-                 incident_id=incident_id)
 
-    return intervention_controller.delete_incident(**kwags)
+    return incident_controller.delete_incident(incident_type, incident_id)
 
 
-@app.route('/api/v1/interventions/<int:incident_id>/escalate', methods=['PATCH'])
+@app.route('/api/v1/interventions/<int:incident_id>/escalate',
+           methods=['PATCH'])
 @jwt_required
 def escalate_intervention(incident_id):
-    user_id = get_jwt_identity()
-    kwags = dict(user_id=user_id, incident_type=incident_type,
-                 incident_id=incident_id)
 
-    return intervention_controller.escalate_incident(**kwags)
+    return incident_controller.escalate_incident(incident_type,
+                                                     incident_id)
